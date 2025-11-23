245a3e1ce92d2ddaefa12533dd4a0ec1aff419de
cgpinta <pinta.christian@outlook.com>
2025-11-22 19:07:34 -0600

    added message box.

---
+++ 20-seconds/scenes/button_hint.gd
@@ -0,0 +1,67 @@
+extends TextureRect
+class_name ButtonHint
+
+var texUnpressed: CompressedTexture2D = preload("res://sprites/key 32pt.png")
+var texPressed: CompressedTexture2D = preload("res://sprites/key down 32pt.png")
+var lblKey: Label
+var lblContainer: CenterContainer
+
+const _unpressedY: float = -2
+const _pressedY: float = 3
+const _pressedScaleY: float = 0.8
+
+@export var actionName: String = ""
+
+var idleyAnimate: bool = false
+var animatedWhenPressed: bool = true
+var idleTimer: float = 0
+const IDLE_TIME_PER_FRAME: float = 1
+
+
+func _ready() -> void:
+	lblKey = $CenterContainer/Label
+	lblContainer = $CenterContainer
+	set_action(actionName)
+	pass
+
+func set_action(str:String):
+	actionName = str
+	lblKey.text = InputMap.action_get_events(actionName)[0].as_text_physical_keycode()
+	pass
+
+func _process(delta: float) -> void:
+	if animatedWhenPressed:
+		if actionName != null and actionName != "":
+			if InputMap.has_action(actionName):
+				if Input.is_action_pressed(actionName):
+					press()
+					return
+				else:
+					unpress()
+		
+		pass
+	if idleyAnimate:
+		if idleTimer < IDLE_TIME_PER_FRAME:
+			idleTimer += delta
+		else:
+			idleTimer = 0
+			if get_is_pressed():
+				unpress()
+			else:
+				press()
+	
+	pass
+
+func get_is_pressed():
+	return texture.resource_name == texPressed.resource_name
+
+func press():
+	texture = texPressed
+	lblContainer.position.y = _pressedY
+	lblContainer.scale.y = _pressedScaleY
+	
+	
+func unpress():
+	texture = texUnpressed
+	lblContainer.position.y = _unpressedY
+	lblContainer.scale.y = 1
---
+++ 20-seconds/scenes/textbox.gd
@@ -0,0 +1,150 @@
+extends Control
+class_name Textbox
+
+enum Mode {
+	Instant,
+	PerChar
+}
+
+class TextboxMsgInfo:
+	var username: String = ""
+	var text: String = ""
+	var mode: Mode = Mode.PerChar
+	var forTime: float = 0
+	
+	func _init(username: String, text:String, mode: Mode = Mode.PerChar, forTime: float = 0) -> void:
+		self.username = username
+		self.text = text
+		self.mode = mode
+		self.forTime = forTime
+		
+
+const TIME_PER_CHAR: float = 0.01
+var charTimer: float = 0
+
+var mode: Mode
+
+var lblText: RichTextLabel
+var buttonHint: ButtonHint
+
+var allowSkipInput: bool = true
+var isActive: bool = false
+
+var messageQueue: Array[TextboxMsgInfo] = []
+var currentDestText: String = ""
+var currentText: String = ""
+var currentSpeaker: String = ""
+var talkTimer: float = 0
+
+signal textboxClosed
+
+func _ready() -> void:
+	lblText = $"margin text/Text"
+	buttonHint = $"margin icons/key icon"
+	close()
+	
+	var testQueue: Array[TextboxMsgInfo] = [
+		TextboxMsgInfo.new("AGENT", "erm what the frick", Mode.PerChar),
+		TextboxMsgInfo.new("AGENT", "yeah um... that was awkward", Mode.PerChar),
+		TextboxMsgInfo.new("AGENT", "FRICK", Mode.Instant)
+	]
+	add_queue(testQueue)
+	pass
+
+func _process(delta: float) -> void:
+	
+	if isActive:
+		if allowSkipInput:
+			if Input.is_action_just_released("advance_text"):
+				_input_pressed()
+				pass
+		
+		if currentText != currentDestText:
+			if mode == Mode.PerChar:
+				if charTimer < TIME_PER_CHAR:
+					charTimer += delta
+				else:
+					if currentText.length() < currentDestText.length():
+						currentText += currentDestText[currentText.length()]
+						_set_text(currentText)
+						charTimer = 0
+						pass
+					
+		
+		if talkTimer > 0:
+			talkTimer -= delta
+		else:
+			pass
+		pass
+
+func add_queue(messages: Array[TextboxMsgInfo]):
+	messageQueue.append_array(messages)
+	isActive = true
+	_open()
+	_speak_next_message_in_queue()
+
+func _speak_next_message_in_queue() -> bool:
+	if messageQueue.size() > 0:
+		_speak_info(messageQueue[0])
+		messageQueue.remove_at(0)
+		return true
+	return false
+
+func _input_pressed():
+	if not _speak_next_message_in_queue():
+		textboxClosed.emit()
+		close()
+
+func close():
+	visible = false
+	messageQueue.clear()
+	_set_text("")
+	pass
+
+func _open():
+	visible = true
+	pass
+
+func _set_text(text:String):
+	if currentSpeaker != "":
+		lblText.text = currentSpeaker + "> "+ text
+	else:
+		lblText.text = text
+	pass
+
+func speak(mode: Mode, text: String):
+	set_allow_input(true)
+	isActive = true
+	self.mode = mode
+	currentDestText = text
+	match mode:
+		Mode.Instant:
+			_set_text(currentDestText)
+			pass
+		Mode.PerChar:
+			pass
+		pass
+	pass
+
+func _speak_info(info: TextboxMsgInfo):
+	currentSpeaker = info.username
+	currentText = ""
+	if info.forTime != 0:
+		speak_for_time(info.mode, info.text, info.forTime)
+	else:
+		speak(info.mode, info.text)
+	pass
+
+
+func set_allow_input(value: bool):
+	allowSkipInput = value
+	buttonHint.visible = allowSkipInput
+	pass
+
+func speak_for_time(mode: Mode, text: String, time:float):
+	set_allow_input(false)
+	isActive = true
+	self.mode = mode
+	speak(mode, text)
+	await get_tree().create_timer(time, true, false, true).timeout
+	pass
