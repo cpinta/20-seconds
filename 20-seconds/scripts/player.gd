c3c1345ac1389010cd3ee94f4beae448df33dea6
cgpinta <pinta.christian@outlook.com>
2025-11-28 01:30:28 -0600

    player reset now resets their animations and state as well

--- 20-seconds/scripts/player.gd
+++ 20-seconds/scripts/player.gd
@@ -770,6 +770,20 @@ func set_direction(dir: int):
 
 func reset():
 	velocity = Vector2.ZERO
+	imgEars.position.x = 0.0
+	imgEars.position.y = 0.0
+	imgHead.position.y = 0.0
+	imgHand.rotation_degrees = 0
+	FACE_OFFSET_X_BASE * direction
+	imgFace.position.y = 0.0
+	imgBody.position.y = 0.0
+	imgBody.rotation_degrees = 0
+	imgLegs.position.y
+	imgLegs.rotation_degrees = 0
+	
+	isDucking = false
+	isDuckingSlide = false
+	
 	if gun:
 		gun.reset()
 	pass
